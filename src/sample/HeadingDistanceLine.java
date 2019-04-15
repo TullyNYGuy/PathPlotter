beba81149f880b92ab8aed474750ddd17db9838a
TullyNYGuy <glenn_ball@utilitek.com>
2019-04-15 12:55:19 -0400

    Fixed bugs in generation of x,y points from heading distance.

--- src/sample/HeadingDistanceLine.java
+++ src/sample/HeadingDistanceLine.java
@@ -62,30 +62,65 @@ public class HeadingDistanceLine {
         return slope;
     }
 
-    private double maximumX(){
+    private double maximumX(double heading){
         double maximumX = distance/Math.sqrt(1 + Math.pow(Math.tan(Math.toRadians(heading)), 2));
+        if ((heading > 90 && heading < 270) || (heading < -90 && heading > -270)) {
+            maximumX = - maximumX;
+        }
         return maximumX;
     }
 
     private void convertToXY(){
         double y;
+        double maximumX;
+        double adjustedHeading = heading;
         point2DList = new Point2DList();
-        if (driveDirection == DriveDirection.FORWARD) {
-            for (double x = 0; x < maximumX(); x = x + maximumX() / 25){
-                y = Math.tan(Math.toRadians(heading))*x;
-                point2DList.add(new Point2D(x,y));
-            }
-            y = Math.tan(Math.toRadians(heading))*maximumX();
-            point2DList.add(new Point2D(maximumX(),y));
-        } else {
-            // driving backwards
-            for (double x = 0; x > -maximumX(); x = x - maximumX() / 25){
-                y = Math.tan(Math.toRadians(heading))*x;
-                point2DList.add(new Point2D(x,y));
-            }
-            y = Math.tan(Math.toRadians(heading))*-maximumX();
-            point2DList.add(new Point2D(-maximumX(),y));
+
+        // if the robot is driving in reverse, the actual movement is not in the direction of the heading. It is 180
+        // degrees opposite of it. So make the adjustment.
+        if (driveDirection == DriveDirection.REVERSE) {
+            adjustedHeading = heading + 180;
         }
+
+        maximumX = maximumX(adjustedHeading);
+
+        // create a set of x,y points by incrementing x from 0 to maximumX. The for loop has to be different when
+        // a set of points from x = 0 to a Positive is being created than when x=0 to a negative is being created.
+        if (maximumX >= 0) {
+            for (double x = 0; x < maximumX; x = x + maximumX / 25){
+                y = Math.tan(Math.toRadians(heading))*x;
+                point2DList.add(new Point2D(x,y));
+            }
+            // create the last point
+            y = Math.tan(Math.toRadians(heading))*maximumX;
+            point2DList.add(new Point2D(maximumX,y));
+        } else {
+            for (double x = 0; x > maximumX; x = x + maximumX / 25){
+                y = Math.tan(Math.toRadians(heading))*x;
+                point2DList.add(new Point2D(x,y));
+            }
+            // create the last point
+            y = Math.tan(Math.toRadians(heading))*maximumX;
+            point2DList.add(new Point2D(maximumX,y));
+        }
+
+
+//        if (driveDirection == DriveDirection.FORWARD) {
+//            for (double x = 0; x < maximumX(); x = x + maximumX() / 25){
+//                y = Math.tan(Math.toRadians(heading))*x;
+//                point2DList.add(new Point2D(x,y));
+//            }
+//            y = Math.tan(Math.toRadians(heading))*maximumX();
+//            point2DList.add(new Point2D(maximumX(),y));
+//        } else {
+//            // driving backwards
+//            for (double x = 0; x > -maximumX(); x = x - maximumX() / 25){
+//                y = Math.tan(Math.toRadians(heading))*x;
+//                point2DList.add(new Point2D(x,y));
+//            }
+//            y = Math.tan(Math.toRadians(heading))*-maximumX();
+//            point2DList.add(new Point2D(-maximumX(),y));
+//        }
 
     }
 
